aef379577334179fd16d8e474add7da16b5c4fc1
suryakiran594 <120645302+suryakiran594@users.noreply.github.com>
2023-04-09 23:28:18 +0530

    All Work

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,7 @@
 import sys
 from src.logger import logging
 
+
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -19,8 +20,7 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message    
-    
-'''
+
 if __name__=="__main__":
     logging.info("Logging has started")
     try:
@@ -28,5 +28,4 @@ if __name__=="__main__":
     except Exception as e:
         logging.info('Dicision by zero') 
         raise CustomException(e,sys)
-'''
-Footer
+
